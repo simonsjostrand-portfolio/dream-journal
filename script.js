d6fb397468eb1f37cdddf8fe750aeb77c6a925ec
simonsjostrand-portfolio <simon.portfolio.git@gmail.com>
2025-02-16 03:42:18 +0100

    Implement local storage for persisting user dreams

--- script.js
+++ script.js
@@ -1,88 +1,131 @@
 'use strict';
 
-const dreamFormContainer = document.querySelector('.dream_form-container');
-const dreamForm = document.getElementById('dream-form');
-const titleInput = document.getElementById('dream_title-input');
-const dateInput = document.getElementById('dream_date-input');
-const descriptionInput = document.getElementById('dream_description-input');
+const header = document.querySelector('.hero');
+const overlay = document.getElementById('overlay');
+const noteNoDreams = document.querySelector('.note-no-dreams');
+const dreamList = document.querySelector('.dream-list');
+
+const formContainer = document.querySelector('.dream_form-container');
+const form = document.getElementById('dream-form');
+const inputTitle = document.getElementById('dream_title-input');
+const inputDate = document.getElementById('dream_date-input');
+const inputDescription = document.getElementById('dream_description-input');
+
 const btnAddDream = document.querySelector('.btn-add-dream');
 const btnSubmitDream = document.querySelector('.btn-submit-dream');
 const iconCloseForm = document.querySelector('.icon-close-form');
-const dreamList = document.querySelector('.dream-list');
-const overlay = document.getElementById('overlay');
 
-const dreams = [];
+let dreams = [];
 
-const closeFormPrompt = function () {
-  if (descriptionInput.value === '') {
-    dreamForm.reset();
-    overlay.style.display = 'none';
-    dreamFormContainer.style.display = 'none';
+// Save to local storage
+const setLocalStorage = function (dream) {
+  localStorage.setItem('dreams', JSON.stringify(dream));
+};
+
+// Render dreams
+const displayDreams = function (dreams) {
+  dreamList.innerHTML = '';
+
+  if (dreams.length === 0) {
+    noteNoDreams.style.display = 'block';
   } else {
-    const youSure = prompt('Are you sure you want to close? Yes or No');
-    if (youSure === 'Yes' || youSure === 'yes') {
-      dreamForm.reset();
-      overlay.style.display = 'none';
-      dreamFormContainer.style.display = 'none';
+    noteNoDreams.style.display = 'none';
+    btnAddDream.style.marginBlockEnd = '420px';
+  }
+
+  dreams.forEach(function (dream) {
+    const html = `
+      <article class="dream">
+        <header class="dream-info">
+          <p class="dream-date">${dream.date}</p>
+          <h2 class="dream-title">${dream.title}</h2>
+        </header>
+        <p class="dream-description">${dream.description}</p>
+      </article> 
+    `;
+    dreamList.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+// Get data from local storage
+const getLocalStorage = function () {
+  const data = JSON.parse(localStorage.getItem('dreams'));
+
+  if (!data) return;
+
+  dreams = data;
+
+  displayDreams(dreams);
+};
+getLocalStorage();
+
+// Hide and reset dream form
+const hideForm = function () {
+  form.reset();
+  overlay.style.display = 'none';
+  formContainer.style.display = 'none';
+};
+
+// Close form (ask if not empty)
+const closeForm = function () {
+  // Close if textarea is empty
+  if (inputDescription.value === '') {
+    hideForm();
+  } else {
+    if (
+      confirm(
+        'Poof! Your dream will vanish into the void. Still want to close?'
+      )
+    ) {
+      hideForm();
     }
   }
 };
 
-// Open form (add dream)
+// Open dream form
 btnAddDream.addEventListener('click', function () {
   overlay.style.display = 'block';
-  dreamFormContainer.style.display = 'block';
+  formContainer.style.display = 'block';
 });
 
-// Submit dream
-dreamForm.addEventListener('submit', function (e) {
+// Submit and display new dream
+form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const newDream = {
     id: Date.now(),
-    title: titleInput.value,
-    date: dateInput.value,
-    description: descriptionInput.value,
+    title: inputTitle.value,
+    date: inputDate.value,
+    description: inputDescription.value,
   };
-
   dreams.push(newDream);
+  setLocalStorage(dreams);
+  displayDreams(dreams);
+  console.log(localStorage);
 
-  // dreamList.innerHTML = '';
-
-  const newCard = document.createElement('article');
-  newCard.classList.add('dream-card');
-  newCard.innerHTML = `
-  <header class="dream-info">
-    <p class="dream-date">${newDream.date}</p>
-    <h2 class="dream-title">${newDream.title}</h2>
-  </header>
-  <p class="dream">${newDream.description}</p>
-`;
-  dreamList.prepend(newCard);
-
-  dreamForm.reset();
-  overlay.style.display = 'none';
-  dreamFormContainer.style.display = 'none';
+  hideForm();
 });
 
-iconCloseForm.addEventListener('click', function () {
-  closeFormPrompt();
+// Close form on click
+iconCloseForm.addEventListener('click', closeForm);
+overlay.addEventListener('click', closeForm);
+
+// Close form on ESC key
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') closeForm();
 });
 
-overlay.addEventListener('click', function () {
-  closeFormPrompt();
-});
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape') closeFormPrompt();
-});
-
-// Fixed icon (go to top)
+// Scroll-to-top (icon)
 const fixedIconWrapper = document.querySelector('.fixed-icon-wrapper');
 const iconFixed = document.querySelector('.icon-go-top');
-const header = document.querySelector('.hero');
 
-window.onscroll = function () {
+iconFixed.addEventListener('click', function (e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// Show scroll icon after header
+window.addEventListener('scroll', function () {
   let headerHeight = header.offsetHeight;
   let scrollPosition = window.scrollY;
 
@@ -91,9 +134,4 @@ window.onscroll = function () {
   } else {
     fixedIconWrapper.classList.remove('visible');
   }
-};
-
-iconFixed.addEventListener('click', function (e) {
-  e.preventDefault();
-  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
